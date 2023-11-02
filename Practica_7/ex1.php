e042885b268a9330607c94635a081648f81b508e
Lumansito <luhmannm0@gmail.com>
2023-11-01 23:02:55 -0300

    Practica 7

--- Practica_7/ex1.php
+++ Practica_7/ex1.php
@@ -1,37 +1,39 @@
 <?php
-$estilo = "estilo1"; // Establece un estilo predeterminado
+$colorFondo = "white"; // Establece un color de fondo predeterminado
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["estilo"])) {
-    // Si se envió el formulario y se eligió un estilo, actualiza la variable $estilo y establece una cookie
-    $estilo = $_POST["estilo"];
-    setcookie("estilo", $estilo, time() + 3600 * 24 * 30); // La cookie expirará en 30 días
-} elseif (isset($_COOKIE["estilo"])) {
-    // Si la cookie de estilo ya existe, obtén el estilo almacenado
-    $estilo = $_COOKIE["estilo"];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["colorFondo"])) {
+   
+    $colorFondo = $_POST["colorFondo"];
+    setcookie("colorFondo", $colorFondo, time() + 3600 * 24 * 30); // La cookie expirará en 30 días
+} elseif (isset($_COOKIE["colorFondo"])) {
+
+    // Si la cookie de color de fondo ya existe, obtén el color de fondo almacenado
+    
+    $colorFondo = $_COOKIE["colorFondo"];
 }
-
-// A continuación, definimos los estilos CSS asociados a cada opción:
-$estilos = array(
-    "estilo1" => "estilo1.css",
-    "estilo2" => "estilo2.css",
-    "estilo3" => "estilo3.css"
-);
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Página con Estilo</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo $estilos[$estilo]; ?>">
+    <title>Página con Color de Fondo</title>
+    <style>
+        body {
+            background-color: <?php echo $colorFondo; ?>;
+        }
+    </style>
 </head>
 <body>
-    <h1>Página con Estilo</h1>
-    <p>Seleccione un estilo para la página:</p>
+    <h1>Página con Color de Fondo</h1>
+    <p>Seleccione un color de fondo para la página:</p>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <label><input type="radio" name="estilo" value="estilo1" <?php echo $estilo === 'estilo1' ? 'checked' : ''; ?>> Estilo 1</label><br>
-        <label><input type="radio" name="estilo" value="estilo2" <?php echo $estilo === 'estilo2' ? 'checked' : ''; ?>> Estilo 2</label><br>
-        <label><input type="radio" name="estilo" value="estilo3" <?php echo $estilo === 'estilo3' ? 'checked' : ''; ?>> Estilo 3</label><br>
-        <input type="submit" value="Guardar Estilo">
+        <select name="colorFondo">
+            <option value="white" <?php if ($colorFondo === 'white') echo 'selected'; ?>>Blanco</option>
+            <option value="lightblue" <?php if ($colorFondo === 'lightblue') echo 'selected'; ?>>Azul Claro</option>
+            <option value="lightgreen" <?php if ($colorFondo === 'lightgreen') echo 'selected'; ?>>Verde Claro</option>
+            <option value="lightpink" <?php if ($colorFondo === 'lightpink') echo 'selected'; ?>>Rosa Claro</option>
+        </select>
+        <input type="submit" value="Guardar Color de Fondo">
     </form>
 </body>
 </html>
